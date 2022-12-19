dd8936ab1e1141af1d69659dc5caac1ede562c22
Kevin Trinh <trinh_thanh42@yahoo.com>
2022-12-19 15:19:10 -0600

    added npm run build script to deploy

--- backend/app.js
+++ backend/app.js
@@ -4,42 +4,42 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
+var dotenv = require('dotenv')
+
 var usersRouter = require('./routes/users');
 var postsRouter = require('./routes/posts');
 var commentsRouter = require('./routes/comments');
 
-var app = express();
+const app = express()
+const port = process.env.PORT || 4000
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+dotenv.config()
 
-app.use('/', indexRouter);
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+
+
 app.use('/api/users', usersRouter);
 app.use('/api/posts', postsRouter);
 app.use('/api/comments', commentsRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(express.static('/Users/kevin/Desktop/reactjs/Blog-app/frontend/build'))
+app.get('*', (req,res) => res.sendFile('/Users/kevin/Desktop/reactjs/Blog-app/frontend/build/index.html'))
+console.log(__dirname)
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+app.use((err, req, res, next) => {
+    res.status(500).send({ message: err.message})
+})
+
+
+app.listen(port, () => {
+    console.log(`Server Listening at http://localhost:${port}`)
+})
 
 module.exports = app;
+
+
